936ef6cf959dacaae33388ae7b0f1a6e9a7b62d7
LMcGart <92118030+LMcGart@users.noreply.github.com>
2023-09-18 16:20:33 -0400

    nav created

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,16 +1,13 @@
-import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Projects() {
   return (
     <main className="h-screen flex items-center justify-center py-10 px-12 fixed top-0 left-0 right-0 bottom-0">
       <div className="h-full w-screen border-[2px] border-solid rounded-xl border-dark-grey overflow-hidden">
         <div className="font-mondwest text-[48px] leading-none px-[30px] py-[50px] relative h-full">
-          Liam Mc Gartoll
+          Gallery
           <br />
-          <div className="font-bit text-2xl leading-none">
-            Designer + Developer
-          </div>
+          <div className="font-bit text-2xl leading-none">+++</div>
           <ul className="font-bit text-2xl list-square pl-[13px] pt-6">
             <li>
               <Link href="/">Home</Link>
@@ -25,11 +22,6 @@ export default function Home() {
               <Link href="/gallery">Gallery</Link>
             </li>
           </ul>
-          <div className="font-bit absolute bottom-[50px] right-[30px] w-[640px] text-[32px] text-right">
-            Hi! I’m Liam, a UX Design and Computer Science student at the
-            University of Michigan who is passionate about web development and
-            creating beautiful things that are accessible to all.
-          </div>
         </div>
       </div>
     </main>
